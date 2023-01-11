ea4cbcc7a7ce191e2e3788348e9bd5a151253768
Nasrullah <nasrullah01n@gmail.com>
2023-01-11 21:48:38 +0800

    Added shimmer effect to details screen

--- feature/restaurant/src/main/java/com/nasportfolio/restaurant/details/RestaurantDetailsScreen.kt
+++ feature/restaurant/src/main/java/com/nasportfolio/restaurant/details/RestaurantDetailsScreen.kt
@@ -1,10 +1,12 @@
 package com.nasportfolio.restaurant.details
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.*
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
@@ -38,9 +40,11 @@ import androidx.navigation.NavHostController
 import com.nasportfolio.common.components.CltButton
 import com.nasportfolio.common.components.CltHeading
 import com.nasportfolio.common.components.CltInput
+import com.nasportfolio.common.components.CltShimmer
 import com.nasportfolio.common.navigation.homeScreenRoute
 import com.nasportfolio.common.navigation.navigateToHomeScreen
 import com.nasportfolio.common.theme.mediumOrange
+import com.nasportfolio.domain.restaurant.TransformedRestaurant
 import com.nasportfolio.restaurant.details.components.*
 import kotlinx.coroutines.launch
 
@@ -70,6 +74,10 @@ fun RestaurantDetailsScreen(
         }
     }
 
+    LaunchedEffect(state.isLoading) {
+        isScrollEnabled = !state.isLoading
+    }
+
     BackHandler(enabled = true) {
         if (!state.isUpdated) return@BackHandler run {
             navController.popBackStack()
@@ -91,95 +99,195 @@ fun RestaurantDetailsScreen(
             }
         ) {
             state.restaurant?.let { restaurant ->
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(horizontal = 16.dp, vertical = 10.dp)
-                    ) {
-                        DataSection(restaurant = restaurant)
-                        Spacer(modifier = Modifier.height(10.dp))
-                        CltHeading(
-                            text = "Description",
-                            textAlign = TextAlign.Start,
-                            fontSize = 30.sp,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(5.dp))
-                        Text(
-                            text = restaurant.description,
-                            style = MaterialTheme.typography.h6.copy(
-                                fontWeight = FontWeight.Normal
-                            )
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
-                        BranchesSection(
-                            state = state,
-                            setIsScrollEnabled = {
-                                isScrollEnabled = it
-                            },
-                            setIsAnimationDone = {
-                                restaurantDetailsViewModel.onEvent(
-                                    event = RestaurantDetailsEvent.AnimationOverEvent(it)
-                                )
-                            }
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
-                        ReviewMetaDataSection(
-                            restaurant = restaurant,
-                            restaurantDetailsViewModel = restaurantDetailsViewModel
-                        )
-                    }
-                    state.currentUserId?.let { userId ->
-                        AnimatedVisibility(
-                            visible = restaurant.comments.isNotEmpty(),
-                            enter = slideInVertically(
-                                initialOffsetY = { it * 2 },
-                                animationSpec = tween(durationMillis = 500)
-                            ),
-                            exit = fadeOut()
-                        ) {
-                            LazyRow {
-                                item { Spacer(modifier = Modifier.width(16.dp)) }
-                                items(
-                                    count = if (restaurant.comments.size <= 3) {
-                                        restaurant.comments.size
-                                    } else {
-                                        3
-                                    },
-                                ) {
-                                    Row {
-                                        CommentCard(
-                                            modifier = Modifier.width(config.screenWidthDp.dp - 42.dp),
-                                            comment = restaurant.comments[it],
-                                            currentUserId = userId,
-                                            editComment = {
-                                                restaurantDetailsViewModel.onEvent(
-                                                    RestaurantDetailsEvent.OpenEditCommentDialog(
-                                                        index = it
-                                                    )
-                                                )
-                                            },
-                                            deleteComment = {
-                                                restaurantDetailsViewModel.onEvent(
-                                                    RestaurantDetailsEvent.DeleteComment(index = it)
-                                                )
-                                            }
-                                        )
-                                        Spacer(modifier = Modifier.width(10.dp))
-                                    }
-                                }
-                                item { Spacer(modifier = Modifier.width(6.dp)) }
-                            }
-                        }
-                    }
-                    Spacer(modifier = Modifier.height(10.dp))
-                }
-            }
+                ScreenContent(
+                    restaurant = restaurant,
+                    state = state,
+                    isScrollEnabled = isScrollEnabled,
+                    restaurantDetailsViewModel = restaurantDetailsViewModel,
+                    config = config
+                )
+            } ?: LoadingComponent()
         }
     }
 
-    if (state.commentBeingEdited != null) Dialog(
+    if (state.commentBeingEdited != null) EditCommentDialog(
+        state = state,
+        restaurantDetailsViewModel = restaurantDetailsViewModel,
+        config = config
+    )
+}
+
+@Composable
+private fun ScreenContent(
+    restaurant: TransformedRestaurant,
+    state: RestaurantsDetailState,
+    isScrollEnabled: Boolean,
+    restaurantDetailsViewModel: RestaurantDetailsViewModel,
+    config: Configuration
+) {
+    var isScrollEnabled1 = isScrollEnabled
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp, vertical = 10.dp)
+        ) {
+            DataSection(restaurant = restaurant)
+            Spacer(modifier = Modifier.height(10.dp))
+            CltHeading(
+                text = "Description",
+                textAlign = TextAlign.Start,
+                fontSize = 30.sp,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = restaurant.description,
+                style = MaterialTheme.typography.h6.copy(
+                    fontWeight = FontWeight.Normal
+                )
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            BranchesSection(
+                state = state,
+                setIsScrollEnabled = {
+                    isScrollEnabled1 = it
+                },
+                setIsAnimationDone = {
+                    restaurantDetailsViewModel.onEvent(
+                        event = RestaurantDetailsEvent.AnimationOverEvent(it)
+                    )
+                }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            ReviewMetaDataSection(
+                restaurant = restaurant,
+                restaurantDetailsViewModel = restaurantDetailsViewModel
+            )
+        }
+        state.currentUserId?.let { userId ->
+            AnimatedVisibility(
+                visible = restaurant.comments.isNotEmpty(),
+                enter = slideInVertically(
+                    initialOffsetY = { it * 2 },
+                    animationSpec = tween(durationMillis = 500)
+                ),
+                exit = fadeOut()
+            ) {
+                LazyRow {
+                    item { Spacer(modifier = Modifier.width(16.dp)) }
+                    items(
+                        count = if (restaurant.comments.size <= 3) {
+                            restaurant.comments.size
+                        } else {
+                            3
+                        },
+                    ) {
+                        Row {
+                            CommentCard(
+                                modifier = Modifier.width(config.screenWidthDp.dp - 42.dp),
+                                comment = restaurant.comments[it],
+                                currentUserId = userId,
+                                editComment = {
+                                    restaurantDetailsViewModel.onEvent(
+                                        RestaurantDetailsEvent.OpenEditCommentDialog(
+                                            index = it
+                                        )
+                                    )
+                                },
+                                deleteComment = {
+                                    restaurantDetailsViewModel.onEvent(
+                                        RestaurantDetailsEvent.DeleteComment(index = it)
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                        }
+                    }
+                    item { Spacer(modifier = Modifier.width(6.dp)) }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+}
+
+@Composable
+private fun LoadingComponent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 10.dp)
+    ) {
+        Surface(
+            elevation = 4.dp,
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 5.dp, horizontal = 20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                repeat(3) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        CltShimmer(
+                            modifier = Modifier
+                                .width(30.dp)
+                                .height(30.dp)
+                        )
+                        Spacer(modifier = Modifier.height(5.dp))
+                        CltShimmer(
+                            modifier = Modifier
+                                .width(60.dp)
+                                .height(20.dp)
+                        )
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        CltShimmer(
+            modifier = Modifier
+                .width(150.dp)
+                .height(40.dp)
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        repeat(2) {
+            Column {
+                CltShimmer(
+                    modifier = Modifier
+                        .fillMaxWidth(if (it == 0) 1f else 0.6f)
+                        .height(25.dp)
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        CltShimmer(
+            modifier = Modifier
+                .width(150.dp)
+                .height(40.dp)
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        CltShimmer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .border(2.dp, mediumOrange)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+}
+
+@Composable
+private fun EditCommentDialog(
+    state: RestaurantsDetailState,
+    restaurantDetailsViewModel: RestaurantDetailsViewModel,
+    config: Configuration
+) {
+    Dialog(
         properties = DialogProperties(
             dismissOnBackPress = !state.isEditSubmitting,
             dismissOnClickOutside = !state.isEditSubmitting
